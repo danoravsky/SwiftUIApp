89c33b2858372ef1d62afe3d0663d668115963f7
Daniel Oravsky <orad01@gmail.com>
2020-11-22 08:02:38 +0100

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Daniel Oravský on 11/22/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
